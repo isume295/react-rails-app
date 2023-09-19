5b70160e9842c0e1f7585d0f0c5abb1f653c7b33
Sumeya <sumeyaibrahim72@gmail.com>
2023-09-19 14:48:03 +0300

    Add greeting api

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,6 @@
 class MessagesController < ApplicationController
+    def random
+        @greeting = Message.order('RANDOM()').first.greeting
+        render json: { greeting: @greeting }
+      end
 end
